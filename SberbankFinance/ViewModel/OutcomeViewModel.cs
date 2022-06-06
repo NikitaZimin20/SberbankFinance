0ed7bea62ad815a01002f728b9d141194af8549e
NikitaZimin20 <nikitazimin@mail.ru>
2022-06-06 19:40:40 +0300

    Добавил CC

--- SberbankFinance/ViewModel/OutcomeViewModel.cs
+++ SberbankFinance/ViewModel/OutcomeViewModel.cs
@@ -13,33 +13,11 @@ namespace SberbankFinance.ViewModel
     internal class OutcomeViewModel:BaseViewModel
     {
        
-       public LiveCharts.Wpf.PieChart Series { get; set; }
         public string[] TypeOfWastes { get; set;}
-        private void InitializeChart()
-        {
-           SeriesCollection psc = new SeriesCollection
-         {
-        new LiveCharts.Wpf.PieSeries
-        {
-            
-        Values = new LiveCharts.ChartValues<decimal> {1,1},
-        },
-        new LiveCharts.Wpf.PieSeries
-        {
-        Values = new LiveCharts.ChartValues<decimal> {3,7},
-        }
-            };
-            foreach (LiveCharts.Wpf.PieSeries ps in psc)
-            {
-                Series.Series.Add(ps);
-            }
-
-        }
+       
         public OutcomeViewModel()
         {
-            Series = new LiveCharts.Wpf.PieChart();
-            InitializeChart();
-            
+                 
             TypeOfWastes = new string[] {"Хуй","Залупа" };
         }
       
