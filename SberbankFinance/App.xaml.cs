9c136b0a748980a73f0d93c78d3a234fbb1fdd8c
NikitaZimin20 <nikitazimin@mail.ru>
2022-06-18 20:01:02 +0300

    В последний раз усовершенствовал валидацию

--- SberbankFinance/App.xaml.cs
+++ SberbankFinance/App.xaml.cs
@@ -18,7 +18,7 @@ namespace SberbankFinance
             NavigationStore navigationStore = new NavigationStore();
             ModalNavigationStore modalNavigationStore = new ModalNavigationStore();
 
-            navigationStore.CurrentViewModel = new HomeViewModel(navigationStore);
+            navigationStore.CurrentViewModel = new LoginViewModel(navigationStore);
          
 
             MainWindow = new MainWindow()
