1fa57c5acb8b13d313de36119a3e5904c8ff1330
Aleksej Yakovlev <aleksejborowy@mail.ru>
2022-06-20 16:04:23 +0300

    последний коммит до третьей мировой войны

--- SberbankFinance/ViewModel/SettingsViewModel.cs
+++ SberbankFinance/ViewModel/SettingsViewModel.cs
@@ -1,23 +1,77 @@
-﻿using SberbankFinance.Commands;
+﻿using Microsoft.Win32;
+using SberbankFinance.Commands;
 using SberbankFinance.Stores;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SberbankFinance.ViewModel
 {
-    internal class SettingsViewModel:BaseViewModel
+    internal class SettingsViewModel : BaseViewModel
     {
         public ICommand NavigateHome { get; }
         public ICommand GoAccountSettingsCommand { get; }
+        public ICommand SetImageCommand { get; }
+
+        private bool _haveimage = false;
+
+        public bool HaveImage
+        {
+            get => _haveimage;
+            set => _haveimage = value;
+        }
 
         public SettingsViewModel(NavigationStore navigationStore)
         {
+            LoadImage();
+            SetImageCommand = new RelayCommand(OnExecuteSetImageCommand, CanExecuteSetImageCommand);
             NavigateHome = new NavigateCommand<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore));
             GoAccountSettingsCommand = new NavigateCommand<AccountSettingsViewModel>(navigationStore, () => new AccountSettingsViewModel(navigationStore));
+        }
+
+        public void LoadImage()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Images", Locator.Data.Id.ToString() + ".jpg");
+            if (File.Exists(path))
+            {
+                ImageSource = BitmapFromUri(new Uri(path));
+                HaveImage = true;
+            }
+        }
+
+        public bool CanExecuteSetImageCommand(object p) => true;
+
+        public void OnExecuteSetImageCommand(object p)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Image Files(*.jpg;)|*.jpg;";
+            if (fd.ShowDialog() == true)
+            {
+                var fileNameToSave = Locator.Data.Id.ToString() + Path.GetExtension(fd.FileName);
+                var imagePath = Path.Combine(Environment.CurrentDirectory, "Images", fileNameToSave);
+                File.Copy(fd.FileName, imagePath, true);
+                LoadImage();
+            }
+        }
+
+        public ImageSource ImageSource { get; set; }
+
+        public static ImageSource BitmapFromUri(Uri source)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = source;
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+            return bitmap;
         }
     }
 }
